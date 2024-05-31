a4cc2ba79aa64e74faa2ae892da4bd8be7aa30e7
AurelienAllenic <aurelien.allenic@gmail.com>
2024-05-31 17:53:15 +0200

    (feat) added 404 and 500 pages with one url to test the 500 and the two templates, the conf inside the urls

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,9 +4,12 @@ from . import views as main_views
 from lettings import views as lettings_views
 from profiles import views as profiles_views
 
+handler404 = main_views.handle_404
+handler500 = main_views.handle_500
 
 urlpatterns = [
     path('', main_views.index, name='index'),
+    path('get_500/', main_views.get_500, name='get_500'),
     path('lettings/', lettings_views.lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', lettings_views.letting, name='letting'),
     path('profiles/', profiles_views.profiles_index, name='profiles_index'),
