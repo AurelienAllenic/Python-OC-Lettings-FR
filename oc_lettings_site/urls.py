a5d531b44d8ffbd17a30d998af51a1d5121c4d88
AurelienAllenic <aurelien.allenic@gmail.com>
2024-06-07 14:14:39 +0200

    (feat) added line for collecting static inside yml file and reception of static elements into urls.py

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views as main_views
 from lettings import views as lettings_views
 from profiles import views as profiles_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 handler404 = main_views.handle_404
 handler500 = main_views.handle_500
@@ -15,4 +17,4 @@ urlpatterns = [
     path('profiles/', profiles_views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', profiles_views.profile, name='profile'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
